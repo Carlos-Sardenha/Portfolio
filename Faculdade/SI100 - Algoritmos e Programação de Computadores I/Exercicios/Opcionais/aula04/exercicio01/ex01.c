eeb5e1a89a1010078814885f606168357a4d9a3f
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-03 12:25:06 -0300

    Update ex01.c

--- Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio01/ex01.c
+++ Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio01/ex01.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <unistd.h> // Necessário para a função sleep
 
+int main(){
+
+    int num[20] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+    for(int i = 0; i < 20; i++){
+        int resultado = num[i] * num[i];
+        printf("%d\n", resultado);    
+        sleep(1);
+    }   
+    
+    return 0;
+}
