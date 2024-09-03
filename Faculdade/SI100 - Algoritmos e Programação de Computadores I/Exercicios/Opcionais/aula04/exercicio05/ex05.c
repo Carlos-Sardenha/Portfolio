532f6dadbebf29efc66ab547f24d58499586603d
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-03 17:11:41 -0300

    Update ex05.c

--- Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio05/ex05.c
+++ Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio05/ex05.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <unistd.h> // Necessário para a função sleep
+
+int main(){
+
+    int num[10];
+    int par = 0;
+    int nao_par = 0;
+    
+    printf("Digite 10 numeros:\n");
+    for(int i = 0; i < 10; i++) {
+       scanf(" %d", &num[i]);
+    }
+
+    for(int i = 0; i < 10; i++){
+        if(num[i] % 2 == 0){
+            par += 1;
+        }else{
+            nao_par += 1;
+        }
+    }
+    
+    printf("Pares:%d \nImpares: %d", par, nao_par);
+    
+    return 0;   
+}
 
