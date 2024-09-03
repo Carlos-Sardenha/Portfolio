aea8aff4d06775d41e5ab5b0615d96083a3295c3
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-03 15:56:13 -0300

    Update ex03.c

--- Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio03/ex03.c
+++ Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio03/ex03.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <unistd.h> // Necessário para a função sleep
+
+int main(){
+
+    int vetor[224]; 
+    int start = 32; 
+
+    
+    for(int i = 0; i < 224; i++) {
+        vetor[i] = start + i;
+    }
+    
+    for(int i = 0; i < 224; i++) {
+        printf("%c\n", vetor[i]);
+    }
+    
+    for(int i = 0; i < 224; i++) {
+        printf("%d\n", i);
+    }
+    
+    for(int i = 0; i < 224; i++) {
+        printf("%X\n", i);
+    }
+    
+    return 0;   
+}
 
