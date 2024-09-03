10fe296f8cb7882ee4887ddd5fe33dd5b22a09d7
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-03 12:34:05 -0300

    Update ex02.c

--- Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio02/ex02.c
+++ Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio02/ex02.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> // Necessário para a função sleep
 
+int main(){
+
+    char caracter = 'a';
+    
+    while(caracter != '#'){
+        printf("Escreva um caracter\n");
+        scanf(" %c", &caracter);
+        printf("Código ASICC é: %d\n", caracter);
+    }
+    return 0;
+}
