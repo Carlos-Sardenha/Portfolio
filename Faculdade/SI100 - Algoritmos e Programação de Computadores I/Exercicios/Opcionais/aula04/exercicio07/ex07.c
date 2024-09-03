923bba0471c1a32adc18906a814aae6ba8693996
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-03 17:44:23 -0300

    Update ex07.c

--- Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio07/ex07.c
+++ Faculdade/SI100 - Algoritmos e Programação de Computadores I/Exercicios/Opcionais/aula04/exercicio07/ex07.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <unistd.h> // Necessário para a função sleep
 
+/*
+    1- 6p
+    2- 4p / 2b
+    3- 6b
+    5- 4p /2b
+    6- 6p
+*/
+
+void imprime_linha(int p, int b) {// ex 4p | 2b 
+    for(int i = 0; i < p/2; i++){//2p
+        printf("⚫");
+    }
+    for(int i = 0; i < b; i++){//2b
+        printf("⚪");
+    }
+    for(int i = 0; i < p/2; i++){//2p
+        printf("⚫");
+    }
+}
+
+int main(){
+
+    for(int i = 0; i < 6; i++){//imprimir linha 1
+        printf("")
+    }
+}
