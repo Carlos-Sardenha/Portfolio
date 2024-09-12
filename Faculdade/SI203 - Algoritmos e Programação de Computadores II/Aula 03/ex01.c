38a4e6c7f180c070a7d794e724377fc7c2e728e8
Carlos Alberto Sardenha FIlho <131621037+Carlos-Sardenha@users.noreply.github.com>
2024-09-12 19:55:09 -0300

    Update ex01.c

--- Faculdade/SI203 - Algoritmos e Programação de Computadores II/Aula 03/ex01.c
+++ Faculdade/SI203 - Algoritmos e Programação de Computadores II/Aula 03/ex01.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+define TAM = 5;
+
+int main (){
+
+  struct band {
+    char nome;
+    char genero;
+    int integrantes;
+    int instagram;
+    int ouvintes;
+  } banda
+  
+  return 0;
+}
